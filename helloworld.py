04021dcd5b1e514b48217af22e600c4c24f1f86d
Michael Robles <dokosatchii@gmail.com>
2018-06-13 21:15:00 -0400

    first commit

--- helloworld.py
+++ helloworld.py
@@ -11,6 +11,7 @@ print(second_string)
 print(first_number)
 print(second_number)
 print(first_and_second_multiplied)
+print("My name is Michael")
 
 #Need to convert numbers to print them with strings
 print(second_string + " " + str(second_number))
